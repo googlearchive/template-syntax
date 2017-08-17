7b02e1edc6608d40a8497c061d70f26cfaf2d3ca
Patrice Chalin <pchalin@gmail.com>
2017-08-16 21:32:53 -0400

    Sync with 6746288
    Synced with dart-lang/site-webdev 4-dev branch, commit 6746288:
    https://github.com/dart-lang/site-webdev/tree/67462888e8be9db94471ba0216ae9e85810c8ce5/examples/ng/doc/template-syntax

--- lib/src/hero_detail_component.dart
+++ lib/src/hero_detail_component.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:angular2/angular2.dart';
+import 'package:angular/angular.dart';
 
 import 'hero.dart';
 
--- lib/src/sizer_component.dart
+++ lib/src/sizer_component.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'dart:math';
-import 'package:angular2/angular2.dart';
+import 'package:angular/angular.dart';
 
 const _minSize = 8;
 const _maxSize = _minSize * 5;
